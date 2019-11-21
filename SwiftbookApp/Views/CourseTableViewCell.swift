af0845966ddde067e36f42e320f07ea2c10f46c6
Alexey Efimov <lex.efimov@gmail.com>
2019-11-21 12:53:48 +0500

    Minor fixes

--- SwiftbookApp/Views/CourseTableViewCell.swift
+++ SwiftbookApp/Views/CourseTableViewCell.swift
@@ -11,9 +11,9 @@ import UIKit
 class CourseTableViewCell: UITableViewCell {
     func configure(with course: Course) {
         textLabel?.text = course.name
-        guard let imageURL = URL(string: course.imageUrl!) else { return }
+        guard let stringURL = course.imageUrl else { return }
+        guard let imageURL = URL(string: stringURL) else { return }
         guard let imageData = try? Data(contentsOf: imageURL) else { return }
         imageView?.image = UIImage(data: imageData)
     }
-
 }
